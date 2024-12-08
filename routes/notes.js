6d40f16a1fa093bea7c3f3c0bec78eae955a3331
ArugaShunki <akatuki589@outlook.jp>
2024-12-09 07:12:06 +0900

    更新

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = "mongodb+srv://shunki:Shellingford4869@cluster0.k4qcx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/',async (req, res) => {
