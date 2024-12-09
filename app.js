17c3031a0dae1d0c9675a7338fe2d30d4a9c839b
ArugaShunki <akatuki589@outlook.jp>
2024-12-10 02:58:05 +0900

    add api.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var halloRouter = require('./routes/hallo');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hallo', halloRouter);
 app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
