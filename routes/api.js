1a207623ee6f44a9f61c25ea1bba1d1ea376f620
ArugaShunki <akatuki589@outlook.jp>
2024-12-10 02:56:08 +0900

    add api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/',async(req,res) => {
-request("https://api.thecatapi.com/v1/images/search",function(error,response,body){
+request("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example",function(error,response,body){
 if(!error && response.statusCode == 200){
 const data = JSON.parse(body);
 res.json(data)
